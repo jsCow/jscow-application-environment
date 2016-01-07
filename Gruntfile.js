74d3817768b34d0899897481e6fce1bf9ed9704b
Mario Linz <Mario Linz>
2016-01-07 22:05:09 +0100

    Update npm package version to 1.0.2 and update of the grunt environment.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,38 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		
 		pkg: grunt.file.readJSON('package.json'),
-
+		
 		clean: {
 			clean: [
 				"dist"
 			]
 		},
-
+		
+		less: {
+			production: {
+				options: {
+					cleancss: true,
+					compress: true,
+					sourceMap: true,
+                    sourceMapFilename: "dist/css/theme-min.css.map",
+                    sourceMapBasepath: "dist/css"
+				},
+				files: {
+					"dist/css/theme-min.css": ["dist/css/less/theme.less"]
+				}
+			}
+		},
+		
 		copy: {
 			main: {
 				files: [
+					{
+	                    expand: true,
+	                    //dot: true,
+	                    cwd: 'src/less',
+	                    src: ['**'],
+	                    dest: 'dist/css/less'
+					},
 					{
 	                    expand: true,
 	                    cwd: 'src/',
@@ -23,6 +45,12 @@ module.exports = function(grunt) {
 	                    cwd: 'node_modules/jquery/dist',
 	                    src: ['jquery.min.js'],
 	                    dest: 'dist/js/jquery'
+					},
+					{
+	                    expand: true,
+	                    cwd: 'node_modules/font-awesome',
+	                    src: ['fonts/**'],
+	                    dest: 'dist'
 					},
 					{
 	                    expand: true,
@@ -38,9 +66,9 @@ module.exports = function(grunt) {
 					},
 					{
 	                    expand: true,
-	                    cwd: 'node_modules/font-awesome',
-	                    src: ['fonts/**'],
-	                    dest: 'dist'
+	                    cwd: 'node_modules/jscow-theme/dist/css/less',
+	                    src: ['**'],
+	                    dest: 'dist/css/less'
 					},
 					grunt.file.expand(['node_modules/jscow-*/src/less']).map(function(cwd) {
 			            return [
@@ -56,22 +84,21 @@ module.exports = function(grunt) {
 			}
 		},
 
-		less: {
-			production: {
-				options: {
-					cleancss: true,
-                    compress: true,
-					yuicompress: true,
-					optimization: 2
-				},
-				files: {
-					"dist/css/theme-min.css": [
-						"dist/css/less/theme.less"
-					]
-				}
+		concat: {
+			options: {
+				separator: ';',
+			},
+			dist: {
+				src: [
+					'node_modules/jscow/dist/jscow/jscow.min.js',
+					'node_modules/jscow-*/src/jscow/components/*.js',
+					'node_modules/jscow-*/dist/jscow/components/*.min.js',
+					'src/jscow/components/*.js',
+				],
+				dest: 'dist/js/jscow/jscow.min.js'
 			}
 		},
-
+		
 		uglify: {
 			options: {
 				mangle: {
@@ -84,25 +111,13 @@ module.exports = function(grunt) {
 				},
 				files: [
 					{
-						'dist/js/app/app.min.js': ['src/app/app.js']
+						'dist/js/app/app.min.js': ['src/app/app.js'],
+						'dist/js/jscow/jscow.min.js': ['dist/js/jscow/jscow.min.js']
 					}
 				]
 			}
 		},
 
-		concat: {
-			options: {
-				separator: ';',
-			},
-			dist: {
-				src: [
-					'node_modules/jscow/dist/jscow/jscow.min.js',
-					'node_modules/jscow-*/dist/jscow/components/*.min.js'
-				],
-				dest: 'dist/js/jscow/jscow.min.js'
-			}
-		},
-		
 		jshint: {
 			options: {
 				curly: true,
@@ -115,10 +130,7 @@ module.exports = function(grunt) {
 					jQuery: true
 				}
 			},
-			all: [
-				'src/app/*.js',
-				'dist/js/**/*.js'
-			]
+			all: ['src/jscow/**/*.js']
 		},
 
 		watch: {
@@ -134,32 +146,66 @@ module.exports = function(grunt) {
 					'clean',
 					'jshint',
 					'copy',
-					'concat',
 					'less',
+					'concat',
 					'uglify'
+					/*,yuidoc'*/
 				],
+			}
+		},
+
+		jshint: {
+			options: {
+				curly: true,
+				eqeqeq: true,
+				eqnull: true,
+				browser: true,
+				maxparams: 4,
+				notypeof: true,
+				globals: {
+					jQuery: true
+				}
+			},
+			all: [
+				'src/app/**/*.js',
+				'src/jscow/**/*.js'
+			]
+		},
+
+		yuidoc: {
+			compile: {
+				name: '<%= pkg.name %>',
+				description: '<%= pkg.description %>',
+				version: '<%= pkg.version %>',
+				url: '<%= pkg.homepage %>',
+				options: {
+					paths: 'src/jscow/components/',
+					//themedir: 'path/to/custom/theme/',
+					outdir: 'docs/'
+				}
 			}
 		}
 		
 	});
 	
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	
+	grunt.loadNpmTasks('grunt-contrib-yuidoc');
+
 	// Default task(s).
 	grunt.registerTask('default', [
 		'clean',
 		'jshint',
 		'copy',
-		'concat',
 		'less',
+		'concat',
 		'uglify'
+		/*,'yuidoc'*/
 	]);
-
-};
+	
+}
--- src/app/app.js
+++ src/app/app.js
@@ -3,7 +3,7 @@ $(function(){
 	var btn1 = jsCow.get(jsCow.res.components.button, { 
 		text: 'Getting started with a first button'
 	});
-
+	
 	btn1.run();
 
 });
